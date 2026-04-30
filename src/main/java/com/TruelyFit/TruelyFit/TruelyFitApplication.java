5a0aa70cf3e363898b966c65351ea49f9dd557b5
Dipawali Mandaokar <dipawalimandaokar@gmail.com>
2026-05-01 01:29:48 +0530

    IC project structure

---
+++ src/main/java/com/TruelyFit/TruelyFit/TruelyFitApplication.java
@@ -0,0 +1,13 @@
+package com.TruelyFit.TruelyFit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruelyFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruelyFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TruelyFit/TruelyFit/TruelyFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TruelyFit.TruelyFit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruelyFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
